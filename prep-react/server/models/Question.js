9c8fad5a621e3e2b47a2bd5348376b16b859bbcb
DiyaGoyal0212 <goyal.diya0212@gmail.com>
2025-03-06 00:03:26 +0530

    commit2

--- prep-react/server/models/Question.js
+++
@@ -1,7 +0,0 @@
-const questionSchema = new mongoose.Schema({
-  role: String,
-  question: String,
-  difficulty: String,  // Add difficulty level to the schema
-});
-
-module.exports = mongoose.model('Question', questionSchema);
--- prep-react/server/models/Video.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const VideoSchema = new mongoose.Schema({
-  videoUrl: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Video", VideoSchema);
